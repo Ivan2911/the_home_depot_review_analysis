f64db569c997a6d9a7ead9fc867bad57886e0f02
Ivandros Awom <norvin2911@gmail.com>
2022-11-02 22:23:49 -0500

    rendering new html file

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session
 
 application = Flask(__name__)
 
 @application.route('/')
 def home():
-    return 'AWS kawan!! Banyak Buaya'
+    return render_template('home.html')
 
 if __name__ == "__main__":
-    application.run()
+    application.run(debug = True)
